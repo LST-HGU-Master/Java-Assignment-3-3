ec0f144b549d362abd5a294554a0c41ba5fb16c0
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-04-29 20:36:40 +0900

    Update Prog33Test.java

--- src/test/java/Prog33Test.java
+++ src/test/java/Prog33Test.java
@@ -1,71 +1,86 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import java.io.*;
 
 public class Prog33Test {
+    InputStream originalIn;
+    PrintStream originalOut;
+    ByteArrayOutputStream bos;
+    StandardInputStream in;
 
+    @BeforeEach
+    void before() {
+        //back up binding
+        originalIn  = System.in;
+        originalOut = System.out;
+        //modify binding
+        bos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bos));
+        
+        in = new StandardInputStream();
+        System.setIn(in);
+    }
+    
+    @AfterEach
+    void after() {
+       System.setOut(originalOut);
+       System.setIn(originalIn);
+    }
+    
     @Test
     public void testNumLines()
     {
-        PrintStream originalOut = System.out;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(bos));
-
         // action
         Prog33.main(new String[]{"100"});
 
         // assertion
-        String[] prints = bos.toString().split("\n");
-        assertEquals(100, prints.length);
-
-        // undo the binding in System
-        System.setOut(originalOut);
+        String[] prints = bos.toString().split(System.lineSeparator());
+        try {
+            assertEquals(100, prints.length,"縦の文字数が実行時引数で与えられた値と一致しません!");
+        } catch (AssertionError err) {
+            after();
+            throw err;
+        }
     }
 
     @Test
     public void testNumColumns()
     {
-        PrintStream originalOut = System.out;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(bos));
-
         // action
         Prog33.main(new String[]{"130"});
 
         // assertion
-        String[] prints = bos.toString().split("\n");
-        assertEquals(130, prints[0].length());
-
-        // undo the binding in System
-        System.setOut(originalOut);
+        String[] prints = bos.toString().split(System.lineSeparator());
+        try {
+            assertEquals(130, prints[0].length(),"横の文字数が実行時引数で与えられた値と一致しません!");
+        } catch (AssertionError err) {
+            after();
+            throw err;
+        }        
     }
 
     @Test
     public void testNoAtmarkFirstLine()
     {
-        PrintStream originalOut = System.out;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(bos));
-
         // action
         Prog33.main(new String[]{"28"});
 
         // assertion
-        String[] prints = bos.toString().split("\n");
-        assertFalse(prints[0].contains("@"));
-
-        // undo the binding in System
-        System.setOut(originalOut);
+        String[] prints = bos.toString().split(System.lineSeparator());
+        try {
+            assertFalse(prints[0].contains("@"),"四角形の一番上に＠が含まれています!");
+        } catch (AssertionError err) {
+            after();
+            throw err;
+        }        
     }
 
     @Test
     public void testAllOutputs()
     {
-        PrintStream originalOut = System.out;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(bos));
-
         // action
         Prog33.main(new String[]{"3"});
 
@@ -75,12 +90,15 @@ public class Prog33Test {
                 "＠＊＊",
                 "＠＠＊"
         };
-        String[] prints = bos.toString().split("\n");
-        assertEquals(expected[0], prints[0]);
-        assertEquals(expected[1], prints[1]);
-        assertEquals(expected[2], prints[2]);
-
-        // undo the binding in System
-        System.setOut(originalOut);
+        String[] prints = bos.toString().split(System.lineSeparator());
+        try {
+            assertEquals(expected[0], prints[0]);
+            assertEquals(expected[1], prints[1]);
+            assertEquals(expected[2], prints[2]);
+        } catch (AssertionError err) {
+            after();
+            AssertionError asErr = new AssertionError("＠と＊はどちらも全角文字であることが必要です!");
+            throw asErr;
+        }
     }
 }
