fe10b77c6827474faa921f80782beec3241d49f2
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-05-10 10:23:48 +0900

    Update Prog33.java

--- src/main/java/Prog33.java
+++ src/main/java/Prog33.java
@@ -1,18 +1,18 @@
 public class Prog33 {
 
-	public static void main(String[] args) {
-		for (int i=0; i<7; i++) {
-			int j;
-			for(j=0; j<=i; j++) {
-				System.out.print("＊");
-			}
-			for(; j<7; j++) {
-				System.out.print("　");
-			}
+    public static void main(String[] args) {
+        for (int i=0; i<7; i++) {
+            int j;
+            for(j=0; j<=i; j++) {
+                System.out.print("＊");
+            }
+            for(; j<7; j++) {
+                System.out.print("　");
+            }
 
-			System.out.println("");
-		}
-	}
+            System.out.println("");
+        }
+    }
 
 }
 
